22d3b038da255292b94db88b43d883efdd10bdde
Ramsha Khalid <38017359+kramsha00@users.noreply.github.com>
2024-06-01 00:58:49 +0300

    Remaining changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Component from "./Component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const someTestStyle = {
+		background : 'cyan',
+		color: 'black',
+		fontSize: '30px',
+		fontWeight: 'bold',
+		padding: '10px'
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Component name='Ramsha' />
+				<img src={logo} className="App-logo" alt="logo" />
+				<h5>This is my to-do list project to learn react from Laracasts</h5>
+				<p style={someTestStyle}>some work to do</p>
+				<span style={{background: 'blue', padding: '10px'}}>some more testing</span>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Component(props) {
+
+    const [count, setCount] = useState(0);
+
+    function decrement() {
+		setCount(prevCount => prevCount - 1)
+	}
+
+	function increment() {
+		setCount(prevCount => prevCount + 1)
+	}
+
+    return (
+        <div className="p-4">
+            <p>This is {props.name}'s counter!</p>
+            <span className="text-3xl font-bold text-green-600">
+                Count: {count}
+            </span>
+            <div className="flex flex-row gap-4">
+                <button 
+                onClick={decrement}
+                className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded">
+                    -
+                </button>
+                <button 
+                onClick={increment}
+                className="bg-green-500 hover:bg-green-300 text-white font-bold py-2 px-4 rounded">
+                    +
+                </button>
+            </div>
+        </div>
+    );
+}
